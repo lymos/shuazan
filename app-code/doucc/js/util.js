24061bfd2cced2e2113c47682d593d9ba9f1c8ba
lymos <lymos@lymosdeMacBook-Pro.local>
2021-05-22 22:02:31 +0800

    update

--- app-code/doucc/js/util.js
+++ app-code/doucc/js/util.js
@@ -2,7 +2,7 @@ var util = {
 	options: {
 		ACTIVE_COLOR: "#007aff",
 		NORMAL_COLOR: "#000",
-		subpages: ["html/tab-webview-subpage-chat.html", "html/tab-webview-subpage-contact.html"]
+		subpages: ["html/tab-webview-subpage-chat.html", "html/tab-gain.html", "html/tab-me.html"]
 	},
 	/**
 	 *  简单封装了绘制原生view控件的方法
