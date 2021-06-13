acb557a876fad564aca290111d343d61e97920c9
lymos <lymos@lymosdeMacBook-Pro.local>
2021-06-13 20:32:46 +0800

    web rule&qrcode

---
+++ app/controller/Invite.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+use think\facade\View;
+use think\facade\Request;
+use think\facade\Db;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+
+class Invite extends BaseController
+{
+	public function __construct(\think\App $app){
+		parent::__construct($app, false);
+	}
+	
+    public function invite()
+    {
+		$result = Builder::create()
+		    ->writer(new PngWriter())
+		    ->writerOptions([])
+		    ->data('Custom QR code contents')
+		    ->encoding(new Encoding('UTF-8'))
+		    ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+		    ->size(300)
+		    ->margin(10)
+		    ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+		    ->logoPath(__DIR__.'/assets/symfony.png')
+		    ->labelText('This is the label')
+		    ->labelFont(new NotoSans(20))
+		    ->labelAlignment(new LabelAlignmentCenter())
+		    ->build();
+			//error_log(print_r($result, true) . "\r\n", 3, '/Volumes/mac-disk/work/www/debug.log');
+			
+        return View::fetch('invite');
+    }
+
+    
+   
+}
