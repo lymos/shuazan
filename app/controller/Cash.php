f4b36972fad84361f99e79c9be12bf431f2c2a96
lymoslymos@163.com <lymoslymos@163.com>
2021-07-17 21:13:18 +0800

    wxpay part1

--- app/controller/Cash.php
+++ app/controller/Cash.php
@@ -314,6 +314,56 @@ class Cash extends BaseController
 		
 	}
 	
+	public function wxpay(){
+		// 工厂方法构造一个实例
+		$instance = \WeChatPay\Builder::factory([
+		    // 商户号
+		    'mchid' => '1000100',
+		    // 商户证书序列号
+		    'serial' => 'XXXXXXXXXX',
+		    // 商户API私钥 PEM格式的文本字符串或者文件resource
+		    'privateKey' => \WeChatPay\Util\PemUtil::loadPrivateKey('/path/to/mch/apiclient_key.pem'),
+		    'certs' => [
+		        // 可由内置的平台证书下载器 `./bin/CertificateDownloader.php` 生成
+		        'YYYYYYYYYY' => \WeChatPay\Util\PemUtil::loadCertificate('/path/to/wechatpay/cert.pem')
+		    ],
+		    // APIv2密钥(32字节)--不使用APIv2可选
+		    'secret' => 'ZZZZZZZZZZ',
+		    'merchant' => [// --不使用APIv2可选
+		        // 商户证书 文件路径 --不使用APIv2可选
+		        'cert' => '/path/to/mch/apiclient_cert.pem',
+		        // 商户API私钥 文件路径 --不使用APIv2可选
+		        'key' => '/path/to/mch/apiclient_key.pem',
+		    ],
+		]);
+		error_log(print_r($instance, true) . "\r\n", 3, '/Volumes/mac-disk/work/www/debug.log');
+		
+		try {
+		    $resp = $instance->v3->pay->transactions->native->post(['json' => [
+		        'mchid' => '1900006XXX',
+		        'out_trade_no' => 'native12177525012014070332333',
+		        'appid' => 'wxdace645e0bc2cXXX',
+		        'description' => 'Image形象店-深圳腾大-QQ公仔',
+		        'notify_url' => 'https://weixin.qq.com/',
+		        'amount' => [
+		            'total' => 1,
+		            'currency' => 'CNY'
+		        ],
+		    ]]);
+		
+		    echo $resp->getStatusCode() . ' ' . $resp->getReasonPhrase(), PHP_EOL;
+		    echo $resp->getBody(), PHP_EOL;
+		} catch (Exception $e) {
+		    // 进行错误处理
+		    echo $e->getMessage(), PHP_EOL;
+		    if ($e instanceof \Psr\Http\Message\ResponseInterface && $e->hasResponse()) {
+		        echo $e->getResponse()->getStatusCode() . ' ' . $e->getResponse()->getReasonPhrase(), PHP_EOL;
+		        echo $e->getResponse()->getBody();
+		    }
+		}
+
+	}
+	
 	public function alipay(){
 		$alipay_path = BASE_PATH . '/extend/alipay/';
 		require_once $alipay_path . 'wappay/service/AlipayTradeService.php';
