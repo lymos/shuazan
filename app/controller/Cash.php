e8c6934bfcdd2e1bcf25b866f8907c80f76553e4
lymos <lymos@lymosdeMacBook-Pro.local>
2021-07-17 14:48:52 +0800

    alipay part1

--- app/controller/Cash.php
+++ app/controller/Cash.php
@@ -198,6 +198,52 @@ class Cash extends BaseController
 		return $order;
 	}
 	
+	public function alipayReturn(){
+		
+	}
+	
+	public function alipayNotify(){
+		
+	}
+	
+	public function alipay(){
+		$alipay_path = BASE_PATH . '/extend/alipay/';
+		require_once $alipay_path . 'wappay/service/AlipayTradeService.php';
+		require_once $alipay_path . 'wappay/buildermodel/AlipayTradeWapPayContentBuilder.php';
+		require $alipay_path . 'config.php';
+		if (!empty($_POST['WIDout_trade_no'])&& trim($_POST['WIDout_trade_no'])!=""){
+		    //商户订单号，商户网站订单系统中唯一订单号，必填
+		    $out_trade_no = $_POST['WIDout_trade_no'];
+		
+		    //订单名称，必填
+		    $subject = $_POST['WIDsubject'];
+		
+		    //付款金额，必填
+		    $total_amount = $_POST['WIDtotal_amount'];
+		
+		    //商品描述，可空
+		    $body = $_POST['WIDbody'];
+		
+		    //超时时间
+		    $timeout_express="1m";
+		
+		    $payRequestBuilder = new \AlipayTradeWapPayContentBuilder();
+		    $payRequestBuilder->setBody($body);
+		    $payRequestBuilder->setSubject($subject);
+		    $payRequestBuilder->setOutTradeNo($out_trade_no);
+		    $payRequestBuilder->setTotalAmount($total_amount);
+		    $payRequestBuilder->setTimeExpress($timeout_express);
+		    $payResponse = new \AlipayTradeService($config);
+			
+		    $result=$payResponse->wapPay($payRequestBuilder, $config['return_url'], $config['notify_url']);
+		error_log(print_r($result, true) . "\r\n", 3, '/Volumes/mac-disk/work/www/debug.log');
+		    return ;
+		}
+		
+		// View::assign('invite_code', $invite_code);
+		return View::fetch('alipay');
+	}
+	
 	/**
 	 * 付款回调
 	 */
