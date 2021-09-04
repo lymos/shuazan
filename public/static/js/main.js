9d263287027f6dd25c409f9a57e789ad5ec7fb47
lymoslymos@163.com <lymoslymos@163.com>
2021-09-05 00:06:48 +0800

    无痕模式cookie

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -5,9 +5,13 @@ var config = {
 var isLogin = function(path, retect) {
 	// 验证过期时间 过期则删除所有
 	// localStorage.getItem("expire");
-	
-	var token = localStorage.getItem("token"),
-		token_expire = localStorage.getItem("token_expire");
+	if(! isLocalStorageSupported()){
+		var token = getCookie("token"),
+			token_expire = getCookie("token_expire");
+	}else{
+		var token = localStorage.getItem("token"),
+			token_expire = localStorage.getItem("token_expire");
+	}
 	if (token == "" || !token) {
 		if(retect){
 			goto('index.php?s=home/login');
@@ -198,9 +202,16 @@ if (typeof $link_box != null && $link_box) {
 }
 
 var getToken = function() {
-	var token = localStorage.getItem("token"),
-		userid = localStorage.getItem("userid"),
-		token_expire = localStorage.getItem("token_expire");
+	if(! isLocalStorageSupported()){
+		var token = getCookie("token"),
+			userid = getCookie("userid"),
+			token_expire = getCookie("token_expire");
+	}else{
+		var token = localStorage.getItem("token"),
+			userid = localStorage.getItem("userid"),
+			token_expire = localStorage.getItem("token_expire");
+	}
+	
 	return {
 		userid: userid,
 		token: token,
@@ -213,6 +224,9 @@ var gotoLogin = function() {
 };
 
 var setLogin  = function(token){
+	if(! isLocalStorageSupported()){
+		return setCookies(token);
+	}
 	localStorage.setItem("token", token.token);
 	localStorage.setItem("token_expire", token.token_expire);
 	localStorage.setItem("userid", token.userid);
@@ -220,7 +234,65 @@ var setLogin  = function(token){
 	// localStorage.setItem("expire", token.expire);
 };
 
+var setCookies = function(token){
+	setCookie("token", token.token, 5);
+	setCookie("token_expire", token.token_expire, 5);
+	setCookie("userid", token.userid, 5);
+}
+
 var delToken = function(){
-	localStorage.clear();
+	if(! isLocalStorageSupported()){
+		removeCookie("token");
+		removeCookie("token_expire");
+		removeCookie("userid");
+	}else{
+		localStorage.clear();
+	}
+	
 	gotoLogin();
 };
+
+/*设置cookie*/
+function setCookie(name, value, iDay)
+{
+  var oDate=new Date();
+  oDate.setDate(oDate.getDate()+iDay);
+  document.cookie=name+'='+value+';expires='+oDate;
+};
+/*使用方法：setCookie('user', 'simon', 11);*/
+/*获取cookie*/
+function getCookie(name)
+{
+  var arr=document.cookie.split('; '); //多个cookie值是以; 分隔的，用split把cookie分割开并赋值给数组
+  for(var i=0;i<arr[i].length;i++) //历遍数组
+  {
+    var arr2=arr[i].split('='); //原来割好的数组是：user=simon，再用split('=')分割成：user simon 这样可以通过arr2[0] arr2[1]来分别获取user和simon 
+    
+	if(arr2[0]==name) //如果数组的属性名等于传进来的name
+    {
+      return arr2[1]; //就返回属性名对应的值
+    }
+    
+  }
+  return ''; //没找到就返回空
+};
+/*使用方法：getCookie('user')*/
+/*删除cookie*/
+function removeCookie(name)
+{
+	setCookie(name, "", -1); //-1就是告诉系统已经过期，系统就会立刻去删除cookie
+};
+/*使用方法：removeCookie('user')*/
+
+
+function isLocalStorageSupported() {
+    var testKey = 'test',
+        storage = window.sessionStorage;
+    try {
+        storage.setItem(testKey, 'testValue');
+        storage.removeItem(testKey);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
